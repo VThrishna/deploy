397d965fd6846580b92eed2b49df845c06ce9704
V Thrishna <v.thrishna2005@gmail.com>
2025-09-22 11:35:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hi. I am Thrishna. This is my first deployment in react application
+     <h1>Hi. I am Thrishna. This is my first deployment in react application. I am trying to do this.
      </h1>
     </div>
   );
